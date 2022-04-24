a5980d46bad78e44a9c0d3c920b1ade484c88780
Eray Ates <eates23@gmail.com>
2022-04-24 17:43:26 +0200

    fix: minify
    Signed-off-by: Eray Ates <eates23@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import * as path from "path";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import preprocess from "svelte-preprocess";
-import { minifyHtml } from "vite-plugin-html";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [
     svelte({ preprocess: preprocess({ name: "scss" }) }),
-    process.env.NODE_ENV == "production" ? minifyHtml() : null,
+    createHtmlPlugin({
+      minify: process.env.NODE_ENV == "production",
+    }),
   ],
   resolve: {
     alias: {
