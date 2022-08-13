40e220eea7cda75c9bb47b37b679635b260acea6
Eray Ates <eates23@gmail.com>
2022-08-13 21:34:50 +0200

    fix: removed id in body
    Signed-off-by: Eray Ates <eates23@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import App from "@/App.svelte";
 import "@/style/global.scss";
+import App from "@/App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+  target: document.body,
 });
 
 export default app;
